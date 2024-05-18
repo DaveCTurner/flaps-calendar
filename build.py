4012a464b7302cd8fccccaa09ce4727a32ebc5e0
David Turner <david.turner@elastic.co>
2024-05-18 10:45:00 +0100

    WIP

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from icalendar import Calendar, Event, vText
 from datetime import datetime
 import pytz
+import json
 
 cal = Calendar()
 cal['prodid']       = vText('-//DCT//DCT//EN')
@@ -9,16 +10,32 @@ cal['name']         = vText('Flash Company Morris')
 cal['x-wr-calname'] = vText('Flash Company Morris')
 cal['color']        = vText('gold')
 
-event = Event()
-event['summary']     = vText('Flash Company Rehearsal')
-event['description'] = vText('Regular rehearsal')
-event['uid']         = vText('2024-05-18')
-event.add('dtstart', datetime(2024, 5, 18, 17, 0, 0, tzinfo=pytz.timezone('Europe/London')))
-event.add('dtend',   datetime(2024, 5, 18, 19, 0, 0, tzinfo=pytz.timezone('Europe/London')))
-event['location']    = vText('Clifton Village Hall, Otley, LS21 2ES')
-event['color']       = vText('gold')
+with open('rehearsals.json', 'r') as f:
+    rehearsals_raw = json.load(f)
 
-cal.add_component(event)
+for rehearsal_date, rehearsal_meta in rehearsals_raw.items():
+    event = Event()
+    event['uid']         = vText(rehearsal_date)
+    summary  = 'Flash Company Rehearsal'
+    location = 'Clifton Village Hall, Otley, LS21 2ES'
+    date_split = rehearsal_date.split('-')
+    start_time = datetime(int(date_split[0]), int(date_split[1]), int(date_split[2]), 17, 0, 0, tzinfo=pytz.timezone('Europe/London'))
+    if 'start' in rehearsal_meta:
+        summary += ' NB START TIME'
+        time_split = rehearsal_meta['start'].split(':')
+        start_time = start_time.replace(hour=int(time_split[0]), minute=int(time_split[1]))
+    if 'location' in rehearsal_meta:
+        location = rehearsal_meta['location']
+        summary += ' NB LOCATION'
+    event['summary']     = vText(summary)
+    # event['description'] = vText('Regular rehearsal')
+
+    event.add('dtstart',  start_time)
+    event.add('duration', vText('PT2H'))
+    event['location']    = vText(location)
+    event['color']       = vText('gold')
+
+    cal.add_component(event)
 
 with open('test.cal', 'wb') as f:
     f.write(cal.to_ical())
