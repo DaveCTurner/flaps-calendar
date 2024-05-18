b3ba730e21bafd5996c542d201575b85c6fae87e
David Turner <david.turner@elastic.co>
2024-05-18 10:21:21 +0100

    WIP

--- build.py
+++ build.py
@@ -3,15 +3,18 @@ from datetime import datetime
 import pytz
 
 cal = Calendar()
-cal['name'] = vText('Flash Company Morris')
+cal['prodid']       = vText('-//DCT//DCT//EN')
+cal['version']      = vText('2.0')
+cal['name']         = vText('Flash Company Morris')
+cal['x-wr-calname'] = vText('Flash Company Morris')
 
 event = Event()
-event['summary'] = vText('Flash Company Rehearsal')
+event['summary']     = vText('Flash Company Rehearsal')
 event['description'] = vText('Regular rehearsal')
-event['uid'] = vText('2024-05-18')
+event['uid']         = vText('2024-05-18')
 event.add('dtstart', datetime(2024, 5, 18, 17, 0, 0, tzinfo=pytz.timezone('Europe/London')))
 event.add('dtend',   datetime(2024, 5, 18, 19, 0, 0, tzinfo=pytz.timezone('Europe/London')))
-event['location'] = vText('Clifton Village Hall, Otley, North Yorkshire, LS21 2ES')
+event['location']    = vText('Clifton Village Hall, Otley, North Yorkshire, LS21 2ES')
 
 cal.add_component(event)
 
